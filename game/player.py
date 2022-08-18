a9420184a03addb2f619b58cd87a33218cf92291
ServaisLeon-2TL1 <l.servais@students.ephec.be>
2022-08-19 01:13:37 +0200

    Add pre post plus fixing

--- game/player.py
+++ game/player.py
@@ -2,6 +2,10 @@
 class Player():
 
     def __init__(self, player_id, default_symbol, dbobj, customize = False):
+        """ Créé un joueur sur base du player_id , et du symbol
+                 PRE : L'id doit etre un nombre et ne doit pas etre le meme qu'un autre joueur, le symbol doit éxister
+                 POST : Instancie les valeurs pname et symbol au player
+              """
         self.pname = f"p{player_id}"
         self.symbol = default_symbol
 
@@ -15,4 +19,8 @@ class Player():
                 self.symbol = ssym[0]
 
     def win(self, db):
+        """ Va inscrire dans la db que le joueur a gagné
+                  PRE : la db doit exister
+                  POST : inscris le nom du joueur et rajoute une victoire dans la db
+               """
         db.add_result(self.pname, 1, 0)
